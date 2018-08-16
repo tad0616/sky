9a22a5bf8b854feb894d84feedf15c82cbda19d7
Tad <tad0616@gmail.com>
2018-08-16 21:33:54 +0800

    Update sky.php

--- sky.php
+++ sky.php
@@ -13,24 +13,26 @@ $DB_PASS = '';
 $DB_NAME = '';
 
 //接著建立資料表
-// DROP TABLE IF EXISTS `sky`;
-// CREATE TABLE `sky` (
-//   `id` int(11) unsigned NOT NULL,
-//   `link` varchar(255) NOT NULL,
-//   `page` tinyint(3) unsigned NOT NULL,
-//   `ok` enum('0','1') NOT NULL,
-//   PRIMARY KEY (`id`)
-// ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*
+DROP TABLE IF EXISTS `sky`;
+CREATE TABLE `sky` (
+`id` int(11) unsigned NOT NULL,
+`link` varchar(255) NOT NULL,
+`page` tinyint(3) unsigned NOT NULL,
+`ok` enum('0','1') NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-// DROP TABLE IF EXISTS `sky_pages`;
-// CREATE TABLE `sky_pages` (
-//   `id` int(10) unsigned NOT NULL,
-//   `title` varchar(255) NOT NULL,
-//   `content` mediumtext NOT NULL,
-//   `create_date` datetime NOT NULL,
-//   `count` int(10) unsigned NOT NULL,
-//   PRIMARY KEY (`id`)
-// ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `sky_pages`;
+CREATE TABLE `sky_pages` (
+`id` int(10) unsigned NOT NULL,
+`title` varchar(255) NOT NULL,
+`content` mediumtext NOT NULL,
+`create_date` datetime NOT NULL,
+`count` int(10) unsigned NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+ */
 
 // ------------------------------以下勿動------------------------------
 $db = new mysqli($DB_HOST, $DB_ID, $DB_PASS, $DB_NAME);
